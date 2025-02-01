81c3209b4cbacf13a72208a3454012decc1c1dff
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-02-01 11:57:05 +0000

    Delete 1. Front-End Technologies Basics/06. Playwright/06. Lab directory

--- 1. Front-End Technologies Basics/06. Playwright/06. Lab/12-Playwright-Introduction-Lab-Resources/tests/demo.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
- // Define the test suite
- test('Page has Playwright in title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = await page.title();
-  // Define the test cases
-  expect(title).toBe('Fast and reliable end-to-end testing for modern web apps | Playwright');
- });
