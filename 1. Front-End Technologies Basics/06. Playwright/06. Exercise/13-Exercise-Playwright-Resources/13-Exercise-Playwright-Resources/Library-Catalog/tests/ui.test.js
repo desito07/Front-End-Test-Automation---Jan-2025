c403dca3495eef6626fc191ad3fbba0c057d6929
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-02-06 23:44:09 +0000

    Add files via upload

--- 1. Front-End Technologies Basics/06. Playwright/06. Exercise/13-Exercise-Playwright-Resources/13-Exercise-Playwright-Resources/Library-Catalog/tests/ui.test.js
+++ 1. Front-End Technologies Basics/06. Playwright/06. Exercise/13-Exercise-Playwright-Resources/13-Exercise-Playwright-Resources/Library-Catalog/tests/ui.test.js
@@ -4,7 +4,7 @@ const { test, expect} = require('@playwright/test');
 test('Verify "All Books" link is visible', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
-    const allBooksLink = await page.$(`a[href="/catalog"]`);
+    const allBooksLink = await page.locator('a[href="/catalog"]');
     const isLinkVisible = await allBooksLink.isVisible();
     expect(isLinkVisible).toBe(true);
 });
@@ -13,7 +13,7 @@ test('Verify "All Books" link is visible', async ({ page }) => {
 test('Verify "Login" button is  visible', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
-    const loginButton = await page.$(`a[href="/login"]`);
+    const loginButton = await page.locator('a[href="/login"]');
     const isLoginButtonVisible = await loginButton.isVisible();
     expect(isLoginButtonVisible).toBe(true);
 });
@@ -22,18 +22,32 @@ test('Verify "Login" button is  visible', async ({ page }) => {
 test('Verify "Register" button is  visible', async ({ page }) => {
     await page.goto('http://localhost:3000');
     await page.waitForSelector('nav.navbar');
-    const registerButton = await page.$(`a[href="/register"]`);
+    const registerButton = await page.locator('a[href="/register"]');
     const isRegisterButtonVisible = await registerButton.isVisible();
     expect(isRegisterButtonVisible).toBe(true);
 });
 
 //Verify "All Books" link is visible after user login
 test('Verify "All Books" link is visible after user login', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.goto('http://localhost:3000/login');
+    await page.locator('input[name="email"]', 'peter@abv.bg');
     await page.fill('input[name="password"]', '123456');
     await page.click('input[type="submit"]');
-    const allBooksLinkAfterLogin = await page.$(`a[href="/catalog"]`);
+    const allBooksLinkAfterLogin = await page.locator('a[href="/catalog"]');
     const isAllBooksLinkVisible = await allBooksLinkAfterLogin.isVisible();
     expect(isAllBooksLinkVisible).toBe(true);
+});
+
+//Verify buttons are visible when user is logged in
+test.only('Verify buttons are visible when user is logged in', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+
+    await page.locator('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    //await expect(page.locator('//div[id="user"]')).toBeVisible();
+    const emailElement = await page.locator('//div[id="user"]//span');
+    const isEmailButtonVisible = await emailElement.isVisible();
+    expect(isEmailButtonVisible).toBe(true);
 });
