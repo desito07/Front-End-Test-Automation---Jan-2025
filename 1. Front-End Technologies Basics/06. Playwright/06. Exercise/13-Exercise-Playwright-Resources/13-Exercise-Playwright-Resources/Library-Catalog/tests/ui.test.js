edc2d4d13f5588e8009596c5c499c6ea3e867592
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-02-08 00:09:50 +0000

    Add files via upload

--- 1. Front-End Technologies Basics/06. Playwright/06. Exercise/13-Exercise-Playwright-Resources/13-Exercise-Playwright-Resources/Library-Catalog/tests/ui.test.js
+++ 1. Front-End Technologies Basics/06. Playwright/06. Exercise/13-Exercise-Playwright-Resources/13-Exercise-Playwright-Resources/Library-Catalog/tests/ui.test.js
@@ -30,7 +30,7 @@ test('Verify "Register" button is  visible', async ({ page }) => {
 //Verify "All Books" link is visible after user login
 test('Verify "All Books" link is visible after user login', async ({ page }) => {
     await page.goto('http://localhost:3000/login');
-    await page.locator('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
     await page.fill('input[name="password"]', '123456');
     await page.click('input[type="submit"]');
     const allBooksLinkAfterLogin = await page.locator('a[href="/catalog"]');
@@ -39,15 +39,90 @@ test('Verify "All Books" link is visible after user login', async ({ page }) => 
 });
 
 //Verify buttons are visible when user is logged in
-test.only('Verify buttons are visible when user is logged in', async ({ page }) => {
+test('Verify buttons are visible when user is logged in', async ({ page }) => {
     await page.goto('http://localhost:3000/login');
 
-    await page.locator('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
     await page.fill('input[name="password"]', '123456');
     await page.click('input[type="submit"]');
 
-    //await expect(page.locator('//div[id="user"]')).toBeVisible();
-    const emailElement = await page.locator('//div[id="user"]//span');
+    const emailElement = await page.locator('//div[@id="user"]//span');
+    const myBookButton = await page.locator('//a[text()="My Books"]');
+    const addBookButton = await page.locator('//a[text()="Add Book"]');
+    const logoutButton = await page.locator('//a[text()="Logout"]');
+
+
     const isEmailButtonVisible = await emailElement.isVisible();
+    const isMyBookButtonVisible = await myBookButton.isVisible();
+    const isAddBokButtonVisible = await addBookButton.isVisible();
+    const isLogoutButtonVisible = await logoutButton.isVisible();
+
     expect(isEmailButtonVisible).toBe(true);
+    expect(isMyBookButtonVisible).toBe(true);
+    expect(isAddBokButtonVisible).toBe(true);
+    expect(isLogoutButtonVisible).toBe(true);
 });
+
+//Login with valid credentials
+test('/Login with valid credentials', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    await expect(page.locator('a[href="/catalog"]')).toBeVisible();
+    expect(page.url()).toBe('http://localhost:3000/catalog');
+});
+
+//Login with Empty Input Fields
+test('Login with Emtry Input Fields', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain("All fields are required!");
+        await dialog.accept();
+    });
+
+    await expect(page.locator('a[href="/login"]')).toBeVisible();
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
+
+//Login with Empty Email Input Field
+test('Login with Empty Email Input Fields', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain("All fields are required!");
+        await dialog.accept();
+    });
+
+    await expect(page.locator('a[href="/login"]')).toBeVisible();
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
+
+//Login with Empty Password Input Field
+test('Login with Empty Password Input Fields', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain("All fields are required!");
+        await dialog.accept();
+    });
+
+    await expect(page.locator('a[href="/login"]')).toBeVisible();
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
+
+
