766f9a873ade0be1447cd40005b3f8e1e47f8a70
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-02-22 19:56:03 +0000

    Add files via upload

---
+++ 2. Front-End Test Automation/01. Selenium Introduction/01. Exercise/Numeric Calculator/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Numeric_Calculator
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
