c07e1e9019acaf0f338c6c56f190712a061a0e14
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-03-12 22:52:47 +0000

    Add files via upload

---
+++ 2. Front-End Test Automation/04. Selenium WebDriver POM/04. Lab/WebDriverPOMDemo/SumNumApp/SumNuAppTestsWithoutPOM.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumNumApp
+{
+   public class SumNuAppTestsWithoutPOM
+    {
+        IWebDriver driver;
+
+        private SumNumberPage sumPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            
+            sumPage = new SumNumberPage(driver);
+            sumPage.OpenPage();
+        }
+
+        [Test]
+        public void TestWithCorrectData()
+        {
+            //var result = sumPage.SumTwoNumbers("10", "10");
+            Assert.That(sumPage.SumTwoNumbers("10", "10"), Is.EqualTo("Sum: 20"));
+        }
+
+        [Test]
+        public void TestWithInCorrectData()
+        {
+            //var result = sumPage.SumTwoNumbers("InvalidData", "10");
+            Assert.That(sumPage.SumTwoNumbers("InvalidData", "10"), Is.EqualTo("Sum: invalid input"));
+        }
+
+        /*public void SumToNumbers(string number1, string number2)
+        {
+            sumPage.fieldNum1.SendKeys(number1);
+            sumPage.fieldNum2.SendKeys(number2);
+
+            sumPage.calcButton.Click();
+        }*/
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
---
+++ 2. Front-End Test Automation/04. Selenium WebDriver POM/04. Lab/WebDriverPOMDemo/SumNumApp/SumNumberPage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumNumApp
+{
+    public class SumNumberPage
+    {
+        private readonly IWebDriver driver;
+
+        public SumNumberPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        const string PageUrl = "file:///C:/Users/Raya/OneDrive/Desktop/Desi's%20stuff/Front-End%20Technologies/2.%20Front-End%20Test%20Automation/04.%20Selenium%20WebDriver%20POM/04.%20Lab/Resources/Sum-Num/sum-num.html";
+        public IWebElement fieldNum1 => driver.FindElement(By.XPath("//input[@id='number1']"));
+        public IWebElement fieldNum2 => driver.FindElement(By.XPath("//input[@id='number2']"));
+
+        public IWebElement calcButton => driver.FindElement(By.XPath("//input[@id='calcButton']"));
+
+        public IWebElement resultElement => 
+            driver.FindElement(By.XPath("//div[@id='result']"));
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(PageUrl);
+        }
+
+        public string SumTwoNumbers(string number1, string number2)
+        {
+            fieldNum1.SendKeys(number1);
+            fieldNum2.SendKeys(number2);
+
+            calcButton.Click();
+
+            string result = resultElement.Text;
+            return result;
+
+            
+        }
+    }
+}
