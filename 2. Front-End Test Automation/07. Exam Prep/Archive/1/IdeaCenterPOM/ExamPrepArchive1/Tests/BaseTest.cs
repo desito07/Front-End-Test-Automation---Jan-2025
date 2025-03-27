f109f328540c3f01369926b4be8427314d57bca3
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-03-27 22:08:37 +0000

    Add files via upload

---
+++ 2. Front-End Test Automation/07. Exam Prep/Archive/1/IdeaCenterPOM/ExamPrepArchive1/Tests/BaseTest.cs
@@ -0,0 +1,65 @@
+﻿using ExamPrepArchive1.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V132.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPrepArchive1.Tests
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        public LoginPage loginPage;
+
+        public CreateIdeaPage createIdeaPage;
+
+        public MyIdeasPage myIdeasPage;
+
+        public IdeasReadPage ideasReadPage;
+
+        public IdeasEditPage ideasEditPage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddUserProfilePreference("profile.password_manager_enabled", false);
+            chromeOptions.AddArgument("--disable-search-engine-choice-screen");
+
+            driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            loginPage = new LoginPage(driver);
+            createIdeaPage = new CreateIdeaPage(driver);
+            myIdeasPage = new MyIdeasPage(driver);
+            ideasReadPage = new IdeasReadPage(driver);
+            ideasEditPage = new IdeasEditPage(driver);
+
+            loginPage.OpenPage();
+            loginPage.Login("desi@test.com", "123456");
+
+
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        public string GenerateRandomString(int length)
+        {
+            const string chars = "zxcvbnmlkjhgfdsaqwertyuiop";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
