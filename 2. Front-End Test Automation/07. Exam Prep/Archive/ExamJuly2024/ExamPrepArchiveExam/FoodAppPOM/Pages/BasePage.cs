5864fc10687166774c517fd6be5eac774339d5a2
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-03-29 16:32:15 +0000

    Add files via upload

---
+++ 2. Front-End Test Automation/07. Exam Prep/Archive/ExamJuly2024/ExamPrepArchiveExam/FoodAppPOM/Pages/BasePage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAppPOM.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+        protected static readonly string BaseUrl = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com:85";
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public IWebElement navBarFoody => driver.FindElement(By.XPath("//a[@class='navbar-brand']"));
+
+        public IWebElement signUpButton => driver.FindElement(By.XPath("//a[@href='/User/Register']"));
+
+        public IWebElement logInButton => driver.FindElement(By.XPath("//a[@href='/User/Login']"));
+
+        public IWebElement learnMoreButton => driver.FindElement(By.XPath("//a[@class='btn btn-primary btn-xl rounded-pill mt-5']"));
+    }
+}
---
+++ 2. Front-End Test Automation/07. Exam Prep/Archive/ExamJuly2024/ExamPrepArchiveExam/FoodAppPOM/Tests/FoodTests.cs
@@ -0,0 +1,96 @@
+﻿using FoodAppPOM.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAppPOM.Tests
+{
+    public  class FoodTests : BaseTest
+    {
+
+        public string lastAddedFoodName;
+        public string lastAddedFoodDescription;
+        public string searchField;
+        public string searchButton;
+
+        [Test, Order(1)]
+        public void AddFoodWithInvalidDataTest()
+        {
+            addFoodPage.OpenPage();
+
+            addFoodPage.AddFood("", "", "");
+
+            addFoodPage.AssertErrorMessages();
+        }
+
+        [Test, Order(2)]
+        public void AddFoodWithValidDataTest()
+        {
+
+            lastAddedFoodName = "Food " + GenerateRandomString(5);
+            lastAddedFoodDescription = "Description " + GenerateRandomString(5);
+
+            addFoodPage.OpenPage();
+
+            addFoodPage.AddFood(lastAddedFoodName, "", lastAddedFoodDescription);
+
+            Assert.That(driver.Url, Is.EqualTo(myFoodPage.Url), "Url is not correct");
+
+            Assert.That(myFoodPage.TitleLastFood.Text.Trim(), Is.EqualTo(lastAddedFoodName), "Title not matched");
+        }
+
+        [Test, Order(3)]
+        public void EditLastAddedFoodTest()
+        {
+            myFoodPage.OpenPage();
+
+            myFoodPage.EditButtonLastFood.Click();
+
+            string updatedName = "Updated Name: " + lastAddedFoodName;
+
+            editFoodPage.FoodName.Clear();
+            editFoodPage.FoodName.SendKeys(updatedName);
+            editFoodPage.AddButton.Click();
+
+            Assert.That(driver.Url, Is.EqualTo(myFoodPage.Url), "Not correctly redirected");
+
+            Assert.That(myFoodPage.TitleLastFood.Text.Trim(), Is.EqualTo(lastAddedFoodName), "Name do not match");
+         }
+
+        [Test, Order(4)]
+        public void SearchForFoodTest()
+        {
+            myFoodPage.OpenPage();
+            myFoodPage.searchField.SendKeys(lastAddedFoodName);
+            myFoodPage.searchButton.Click();
+
+            Assert.That(myFoodPage.TitleLastFood.Text.Trim(), Is.EqualTo(lastAddedFoodName), "Name do not match");
+        }
+
+        [Test, Order(5)]
+        public void DeleteLastAddedFoodTest()
+        {
+            myFoodPage.OpenPage();
+
+            myFoodPage.DeleteButtonLastFood.Click();
+
+            bool isLastFoodDeleted = myFoodPage.FoodCards.All(food => food.Text.Contains(lastAddedFoodName));
+
+            Assert.IsFalse(isLastFoodDeleted, "The food was not deleted");
+        }
+
+        [Test, Order(6)]
+        public void SearchForDeletedFoodTest()
+        {
+            myFoodPage.OpenPage();
+            myFoodPage.searchField.SendKeys(lastAddedFoodName);
+            myFoodPage.searchButton.Click();
+
+            Assert.That(myFoodPage.noFoodMessage.Text.Trim(), Is.EqualTo("There are no foods :("), "The message that no foos is not dispalyed as expected.");
+        }
+    }
+}
