4f0870dac36d2d7f4c697b386c5130f84132ef8a
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2025-04-14 09:51:37 +0100

    Add files via upload

---
+++ 2. Front-End Test Automation/07. Exam Prep/Prep2_Foody/ExamPrepArchiveExam/FoodAppPOM/Pages/EditFoodPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAppPOM.Pages
+{
+    public class EditFoodPage : BasePage
+    {
+        public EditFoodPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public string Url = BaseUrl + "/Food/Edit";
+
+        public IWebElement FoodName => driver.FindElement(By.XPath("//input[@id='name']"));
+        public IWebElement FoodDescription => driver.FindElement(By.XPath("//input[@id='description']"));
+
+        public IWebElement FoodPicture => driver.FindElement(By.XPath("//input[@id='url']"));
+
+        public IWebElement AddButton => driver.FindElement(By.XPath("//button[@class='btn btn-primary btn-block fa-lg gradient-custom-2 mb-3']"));
+    }
+}
---
+++ 2. Front-End Test Automation/07. Exam Prep/Prep2_Foody/ExamPrepArchiveExam/FoodAppPOM/Pages/MyFoodPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodAppPOM.Pages
+{
+    internal class MyFoodPage : BasePage
+    {
+        public MyFoodPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public string Url = BaseUrl + "/";
+
+        public ReadOnlyCollection<IWebElement> FoodCards => driver.FindElements(By.XPath("//div[@class='row gx-5 align-items-center']"));
+
+        public IWebElement TitleLastFood => FoodCards.Last().FindElement(By.XPath(".//h2[@class='display-4']"));
+
+        public IWebElement EditButtonLastFood => FoodCards.Last().FindElement(By.XPath(".//a[contains(@href,'/Food/Edit')]"));
+
+        public IWebElement DeleteButtonLastFood => FoodCards.Last().FindElement(By.XPath(".//a[contains(@href,'/Food/Delete')]"));
+
+        public IWebElement searchField => driver.FindElement(By.XPath("//input[@class='form-control rounded mt-5 xl']"));
+
+        public IWebElement searchButton => driver.FindElement(By.XPath("//button[@class='btn btn-primary rounded-pill mt-5 col-2']"));
+
+        public IWebElement noFoodMessage => driver.FindElement(By.XPath("//h2[@class='display-4']"));
+
+        public void OpenPage()
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+    }
+}
